f11154bd9cb7700571537637b6e30d1c3be039bd
Ren Bettendorf <Ren.Bettendorf@arthrex.com>
2019-02-17 18:24:24 -0800

    Part B and most of C complete

--- HW4/pca.py
+++ HW4/pca.py
@@ -1,5 +1,6 @@
 import numpy as np
-from numpy import zeros
+from numpy import zeros, int8
+from numpy import linalg as linalg
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from scipy.spatial.distance import pdist, squareform
@@ -25,7 +26,7 @@ def save_mean_images(images):
     for i in range(10):
         plt.subplot(4, 3, i + 1)
         # https://stackoverflow.com/questions/5086789/python-is-there-an-inverse-for-ndarray-flattenf
-        rgb_image_flat = np.reshape(images[i], (3, -1)).T
+        rgb_image_flat = np.transpose(np.reshape(images[i], (3, -1)))
         rgb_image_squared = np.reshape(rgb_image_flat, (32, 32, 3))
         plt.imshow(rgb_image_squared)
         plt.title(label_json[str(i)])
@@ -40,9 +41,41 @@ def create_classifier_data_and_mean_images(data, labels):
     for i in range(10):
         classifier_data.append(data[np.where(labels[:] == i), :][0])
         mean_classifier_images[i, :] = np.mean(classifier_data[i], axis=0)
-    mean_classifier_images = mean_classifier_images.astype(int)
     return classifier_data, mean_classifier_images
 
+
+def calculate_pca_euclidean(distance_matrix):
+    A = np.eye(10) - np.ones((10, 10)) / 10
+    W = -0.5 * np.dot(np.dot(A, distance_matrix), np.transpose(A))
+    eigen_values_complete, eigen_vectors_complete = linalg.eig(W)
+    # https://stackoverflow.com/questions/16486252/is-it-possible-to-use-argsort-in-descending-order
+    index = eigen_values_complete.argsort()[::-1]    
+    eigen_values_partial = np.sqrt(eigen_values_complete[index])
+    eigen_vectors_partial = eigen_vectors_complete[:, index]
+    lambda_s = np.eye(2)
+    lambda_s[0, 0] = eigen_values_partial[0]
+    lambda_s[1, 1] = eigen_values_partial[1]
+    return np.dot(eigen_vectors_partial[:, :2], lambda_s)
+
+
+def calculate_pca_similarity(classifier_data):
+    D = np.zeros((10,10))
+    for i in range(10):
+        for j in range(10):
+            print(f"i = {i}, j = {j}")
+            pca_i = PCA(n_components=20)
+            pca_i.fit(classifier_data[j])
+            transformed_i = pca_i.transform(classifier_data[i])
+            inversed_i = pca_i.inverse_transform(transformed_i)
+
+            pca_j = PCA(n_components=20)
+            pca_j.fit(classifier_data[i])
+            transformed_j = pca_j.transform(classifier_data[j])
+            inversed_j = pca_j.inverse_transform(transformed_j)
+            D[i, j] = np.mean(np.sum((inversed_j - classifier_data[j])**2, axis=1))
+
+            E_ij = (inversed_i + inversed_j)/2
+    return D
 
 # Data: [10000, 3072] 1024R 1024G 1024B
 data1 = unpickle("data_batch_1")
@@ -70,27 +103,44 @@ complete_labels = np.concatenate((data1_labels, data2_labels, data3_labels, data
 
 classifier_data, mean_classifier_images = create_classifier_data_and_mean_images(complete_dataset, complete_labels)
 
-# save_mean_images(mean_classifier_images)
+# save_mean_images(mean_classifier_images.astype(int))
 
-# plt.figure(2)
-# sum_squared_values = []
-# for i in range(10):
-#     pca = PCA(n_components=20)
-#     pca.fit(classifier_data[i])
-#     dataset_transformed = pca.transform(classifier_data[i])
-#     inversed = pca.inverse_transform(dataset_transformed)
-#     difference = classifier_data[i] - inversed
-#     summed = np.sum(difference**2, axis=1)
-#     sum_squared_error = np.mean(summed)
-#     sum_squared_values.append(sum_squared_error)
-    # print(f"ERROR: {sum_squared_error}")
+plt.figure(2)
+sum_squared_values = []
+for i in range(10):
+    pca = PCA(n_components=20)
+    pca.fit(classifier_data[i])
+    dataset_transformed = pca.transform(classifier_data[i])
+    inversed = pca.inverse_transform(dataset_transformed)
+    sum_squared_error = np.mean(np.sum((inversed - classifier_data[i])**2, axis=1))
+    sum_squared_values.append(sum_squared_error)
+    print(f"ERROR: {sum_squared_error}")
 
-# plt.figure(1, [10, 5])
-# plt.bar(range(10), sum_squared_values, align='center')
-# plt.xticks(range(10), labeled_names)
-# plt.title("Sum Squared Error")
-# plt.savefig('Sum_Squared_Error.png')
-# plt.clf()
+plt.figure(1, [10, 5])
+plt.bar(range(10), sum_squared_values, align='center')
+plt.xticks(range(10), labeled_names)
+plt.title("Sum Squared Error")
+plt.savefig('Sum_Squared_Error.png')
+plt.clf()
 
 distance_matrix = euclidean_distances(mean_classifier_images, mean_classifier_images, squared=True)
 np.savetxt('partb_distances.csv', distance_matrix, delimiter=',')
+pca_euclidean = calculate_pca_euclidean(distance_matrix)
+
+plt.scatter(pca_euclidean[:, 0], pca_euclidean[:, 1])
+for label, x, y in zip(labeled_names, pca_euclidean[:, 0], pca_euclidean[:, 1]):
+    plt.annotate(label, (x, y))
+plt.title("PCA Euclidean")
+plt.savefig("pca_mds_b.png")
+plt.clf()
+
+
+pca_similarity = calculate_pca_similarity(classifier_data)
+
+np.savetxt("partc_distances.csv", pca_similarity, delimiter=',')
+plt.scatter(pca_similarity[:, 0], pca_similarity[:, 1])
+for label, x, y in zip(labeled_names, pca_similarity[:, 0], pca_similarity[:, 1]):
+    plt.annotate(label, (x, y))
+plt.title("PCA Similarity")
+plt.savefig("pca_mds_c.png")
+plt.clf()
