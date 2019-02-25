273cf1b8b27b14a36d027e43377480c001daa277
ARTHREX\rbettendorf <Ren.Bettendorf@arthrex.com>
2019-02-25 12:58:07 -0800

    Storing label data in a dictionary for time being

--- HW5/classifier.py
+++ HW5/classifier.py
@@ -6,8 +6,12 @@ CLASSIFIER_LABELS = ["Brush_Teeth", "Climb_Stairs", "Comb_Hair", "Descend_Stairs
                      "Drink_Glass", "Eat_Meat", "Eat_Soup", "Getup_Bed", "Liedown_Bed",
                      "Pour_Water", "Sitdown_Chair", "Standup_Chair", "Use_Telephone", "Walk"]
 
+labels_data = {}
 for label in CLASSIFIER_LABELS:
     file_list = glob.glob("HMP_Dataset/" + label + "/*.txt")
+    label_data = []
     for file in file_list:
-        data = np.genfromtxt(file, delimiter=" ")
-        print(f"Reading {file} with \n\n{data}")
+        file_data = np.genfromtxt(file, delimiter=" ")
+        label_data.append(file_data)
+        # print(f"Reading {file} with \n\n{file_data}")
+    labels_data[label] = label_data
