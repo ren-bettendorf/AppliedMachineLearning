9b81f53945c3d0d20d060f33a70812adbb3504e8
Ren Bettendorf <Ren.Bettendorf@arthrex.com>
2019-03-12 12:33:00 -0700

    Figured out the stopwords

--- HW7/regression.py
+++ HW7/regression.py
@@ -1,6 +1,11 @@
 import pandas as pd
+from nltk.corpus import stopwords
 
+stop = set(stopwords.words('english')) 
 df = pd.read_csv("yelp_2k.csv", usecols=[3, 5])
 # print(f"{df}")
-df["Review Length"] = df["text"].str.len()
+df["text_length"] = df["text"].str.len()
+# print(f"{df}")
+# https://stackoverflow.com/questions/29523254/python-remove-stop-words-from-pandas-dataframe
+df['text_no_stopwords'] = df['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop]))
 print(f"{df}")
