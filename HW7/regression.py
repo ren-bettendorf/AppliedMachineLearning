c736948a9af45ac89e4ba0ac98a61629cce501cb
renaldorini <renaldorini@gmail.com>
2019-03-23 20:54:39 -0700

    Have punctuation and tokenizing working

--- HW7/regression.py
+++ HW7/regression.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import string
+from collections import Counter
+import nltk
 
 df = pd.read_csv("yelp_2k.csv", usecols=[3, 5])
 # print(f"{df}")
-df["text_length"] = df["text"].str.len()
-# print(f"{df}")
-# https://stackoverflow.com/questions/5486337/how-to-remove-stop-words-using-nltk-or-python
+df["text"] = df["text"].str.lower()
+df["text"] = df["text"].str.replace(r'[^\w\s]+', '')
+print(f"{df}")
+
+# https://stackoverflow.com/a/46786277
+word_dist = df["text"].str.split(expand=True).stack().value_counts()
+print(f"{word_dist}")
