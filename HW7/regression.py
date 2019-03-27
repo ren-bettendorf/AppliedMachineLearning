bf0e90d38d8ea42205b2bc6731f01ccf82cba3d6
renaldorini <renaldorini@gmail.com>
2019-03-26 19:48:18 -0700

    Finished part 2 with predictions. Need to fine tune

--- HW7/regression.py
+++ HW7/regression.py
@@ -1,28 +1,33 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import string
 from nltk.probability import FreqDist
+from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity as cosdis
 
 df = pd.read_csv("yelp_2k.csv", usecols=[3, 5])
 # print(f"{df}")
-df["text"] = df["text"].str.lower()
-df["text"] = df["text"].str.replace(r'[^\w\s]+', '')
-df["text"] = df["text"].str.replace('\r\n', '')
-df["text"] = df["text"].str.replace('\r', '')
-df["text"] = df["text"].str.replace('\r\n\r', '')
-df["text"] = df["text"].str.replace('\r\n\r\n', '')
+df["text_lower"] = df["text"].str.lower()
+df["text_lower"] = df["text_lower"].str.replace(r'[^\w\s]+', '')
+df["text_lower"] = df["text_lower"].str.replace('\r\n', '')
+df["text_lower"] = df["text_lower"].str.replace('\r', '')
+df["text_lower"] = df["text_lower"].str.replace('\r\n\r', '')
+df["text_lower"] = df["text_lower"].str.replace('\r\n\r\n', '')
+df["text_lower"] = df["text_lower"].str.replace('\n', '')
+df["text_lower"] = df["text_lower"].str.replace('\n\n', '')
 
-split_words = df["text"].str.split(" ")
+split_words = df["text_lower"].str.split(" ")
 total_words_list = []
 for review in split_words:
     for word in review:
         # Remove empty words
         if word:
             total_words_list.append(word)
-print(f"{len(total_words_list)}")
+print(f"TOTAL WORDS LENGTH: {len(total_words_list)}")
 freq_dist = FreqDist(total_words_list)
-print(f"{len(freq_dist)}")
+print(f"FREQ DIST LENGTH: {len(freq_dist)}")
 
 word_counts = [word[1] for word in freq_dist.most_common()]
 # plt.figure(1)
@@ -32,17 +37,30 @@ word_counts = [word[1] for word in freq_dist.most_common()]
 # plt.ylabel("Word Count")
 # plt.show()
 
-# After analysis of the image I decided to remove 25 which is roughly when words 
+# After analysis of the image
+# decided to remove 25 which is roughly when words
 # Remove words that only occur once
 stop_words_map = freq_dist.most_common(25)
-stop_words = freq_dist.hapaxes()
+stop_words_list = freq_dist.hapaxes()
 for stop_word in stop_words_map:
-    stop_words.append(stop_word[0])
+    stop_words_list.append(stop_word[0])
 
-cleaned_word_list = [word for word in total_words_list if word not in stop_words]
-print(f"{len(cleaned_word_list)}")
-claned_freq_dist = FreqDist(cleaned_word_list)
-cleaned_word_counts = [word[1] for word in claned_freq_dist.most_common()]
+cv = CountVectorizer(stop_words=stop_words_list)
+transformed_list = cv.fit_transform(df["text_lower"]).toarray()
+target = cv.transform(['Horrible customer service']).toarray()
+
+nn = NearestNeighbors(n_neighbors=20, metric='cosine')
+nn.fit(transformed_list)
+values, index = nn.kneighbors(target)
+values = values.flatten()
+index = index.flatten()
+count = 0
+print("--------TOP SCORES-------")
+for i in index:
+    review = df["text"][i][:200] if len(df["text"][i]) > 200 else df["text"][i]
+    print(f'Score: {values[count]} Review: {review}')
+    count += 1
+
 # plt.figure(2)
 # plt.plot(cleaned_word_counts)
 # plt.title("Word Frequency - Post Processing")
