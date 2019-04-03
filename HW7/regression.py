ec82850b2908c9873f658260841716c8a78ddb60
renaldorini <renaldorini@gmail.com>
2019-04-02 19:07:24 -0700

    Added predictions

--- HW7/regression.py
+++ HW7/regression.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import string
 from nltk.probability import FreqDist
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity as cosdis
@@ -47,19 +49,19 @@ for stop_word in stop_words_map:
 
 cv = CountVectorizer(stop_words=stop_words_list)
 transformed_list = cv.fit_transform(df["text_lower"]).toarray()
-target = cv.transform(['Horrible customer service']).toarray()
+# target = cv.transform(['Horrible customer service']).toarray()
 
-nn = NearestNeighbors(n_neighbors=20, metric='cosine')
-nn.fit(transformed_list)
-values, index = nn.kneighbors(target)
-values = values.flatten()
-index = index.flatten()
-count = 0
-print("--------TOP SCORES-------")
-for i in index:
-    review = df["text"][i][:200] if len(df["text"][i]) > 200 else df["text"][i]
-    print(f'Score: {values[count]} Review: {review}')
-    count += 1
+# nn = NearestNeighbors(n_neighbors=20, metric='cosine')
+# nn.fit(transformed_list)
+# values, index = nn.kneighbors(target)
+# values = values.flatten()
+# index = index.flatten()
+# count = 0
+# print("--------TOP SCORES-------")
+# for i in index:
+#     review = df["text"][i][:200] if len(df["text"][i]) > 200 else df["text"][i]
+#     print(f'Score: {values[count]} Review: {review}')
+#     count += 1
 
 # plt.figure(2)
 # plt.plot(cleaned_word_counts)
@@ -67,3 +69,10 @@ for i in index:
 # plt.xlabel("Word Rank")
 # plt.ylabel("Word Count")
 # plt.show()
+
+star_array = df["stars"].values
+train_text, test_text, train_star, test_star = train_test_split(transformed_list, star_array,
+                                                                test_size=0.1, random_state=0)
+lr = LogisticRegression()
+lr.fit(train_text, train_star)
+test_predictions = lr.predict(test_text)
